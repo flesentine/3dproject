b166c5e0e301214cd1a2ff76300d6ea7ea9c01f5
flesentine <chrisgmcc@gmail.com>
2026-08-29 09:19:34 -0700

    build0: define project domain model

---
+++ src/domain/project.ts
@@ -0,0 +1,63 @@
+export type ISODate = string
+
+export type TaskKind = 'task' | 'milestone' | 'summary'
+export type DependencyType = 'FS' | 'SS' | 'FF' | 'SF'
+
+export interface Workstream {
+  id: string
+  name: string
+  order: number
+}
+
+export interface ProjectTask {
+  id: string
+  name: string
+  workstreamId: string
+  parentId?: string
+  kind: TaskKind
+  start: ISODate
+  finish: ISODate
+  progress: number
+  owner?: string
+  baselineStart?: ISODate
+  baselineFinish?: ISODate
+}
+
+export interface Dependency {
+  id: string
+  fromTaskId: string
+  toTaskId: string
+  type: DependencyType
+  lagDays: number
+}
+
+export interface ProjectModel {
+  id: string
+  name: string
+  statusDate: ISODate
+  workstreams: Workstream[]
+  tasks: ProjectTask[]
+  dependencies: Dependency[]
+}
+
+export interface ProjectDateRange {
+  start: ISODate
+  finish: ISODate
+}
+
+export type ValidationSeverity = 'error' | 'warning'
+
+export interface ValidationIssue {
+  code: string
+  severity: ValidationSeverity
+  message: string
+  taskId?: string
+  dependencyId?: string
+}
+
+export interface ScheduleAnalysis {
+  dateRange: ProjectDateRange
+  upstreamByTask: Map<string, string[]>
+  downstreamByTask: Map<string, string[]>
+  validationIssues: ValidationIssue[]
+}
