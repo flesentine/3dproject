4e991ad59dfdf0ad1ae8b759d8d82b4861a820d6
flesentine <chrisgmcc@gmail.com>
2026-08-29 22:31:48 -0700

    build3: load direct manipulation styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './scenario.css'
+import './directManipulation.css'
 
 const root = document.getElementById('root')
 
